f891265cc8fadcc09fda7e0317338d59bc7d7b62
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-02-26 19:16:47 -0500

    Time: 0 ms (100.00%), Space: 8.4 MB (44.74%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -10,24 +10,24 @@
  * };
  */
 class Solution {
-    
-private:
-    void dfs(TreeNode* root, vector<int> &preorder){
-        
-        if(root==NULL) return;
-        
-        preorder.push_back(root->val);
-        dfs(root->left,preorder);
-        dfs(root->right,preorder);
-    }
-    
-    
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         
-        vector<int> preorder;
-        dfs(root,preorder);
-        return preorder;
+        vector<int> ans;
+        if(root==NULL) return ans;
+        
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            root=st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right!=NULL) st.push(root->right);
+            if(root->left!=NULL) st.push(root->left);
+            
+        }
+        return ans;
         
     }
 };
