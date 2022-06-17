b4f1740fc74f1edc166c63193c0f9444b954a743
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-06-16 20:54:21 -0400

    Time: 0 ms (100.00%), Space: 8.6 MB (98.30%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -6,6 +6,7 @@ public:
         
         for(int i =n; i>=0; i--){
             
+            //when the last digit is not 9
             if(digits[i]!=9){
                 
                 digits[i] = digits[i]+1;
@@ -13,11 +14,13 @@ public:
                 
             }
             
+            //when last digit is 9
             else
                 digits[i]=0;
             
         }
         
+        //when all the digits are 9 
         digits.insert(digits.begin(),1);
         return digits;
         
