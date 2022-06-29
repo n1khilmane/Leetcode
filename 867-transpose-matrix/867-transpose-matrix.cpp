7afa08783653e10fb3bbb1f470d6716f5dfaf2c3
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-06-29 17:31:34 -0400

    Time: 13 ms (80.97%), Space: 10.5 MB (76.73%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        int r = matrix.size();
+        int c = matrix[0].size();
+        
+        vector<vector<int>> res(c,vector<int> (r,0));
+        
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                res[j][i] = matrix[i][j];
+            }
+            
+           
+        } 
+        
+         return res;
+        
+    }
+};
