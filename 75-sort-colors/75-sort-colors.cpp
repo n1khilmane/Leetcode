7285395525790a5891c4322a47a610c05e19b070
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-07-26 14:54:48 -0400

    Time: 0 ms (100.00%), Space: 8.3 MB (70.19%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int lo = 0; 
-        int hi = nums.size() - 1; 
-        int mid = 0; 
-
-        while (mid <= hi) { 
-            switch (nums[mid]) { 
-
-            // If the element is 0 
-            case 0: 
-                swap(nums[lo++], nums[mid++]); 
-                break; 
-
-            // If the element is 1 . 
-            case 1: 
-                mid++; 
-                break; 
-
-            // If the element is 2 
-            case 2: 
-                swap(nums[mid], nums[hi--]); 
-                break; 
+        
+        //consider 3 pointers 
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        
+        
+        // 3 conditions which needs to be checked 
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
-         
+        
     }
 };
