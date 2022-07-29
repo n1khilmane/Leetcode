d07dda7587d0453fe61c1a45354fda26ebdcef89
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-07-29 11:57:55 -0400

    Time: 158 ms (70.53%), Space: 61.1 MB (99.30%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,18 +2,29 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
-          int slow = nums[0];
-  int fast = nums[0];
-  do {
-    slow = nums[slow];
-    fast = nums[nums[fast]];
-  } while (slow != fast);
-  fast = nums[0];
-  while (slow != fast) {
-    slow = nums[slow];
-    fast = nums[fast];
-  }
-  return slow;
+        //linkedlist cycle method / slow pointer and fast pointer approach
+        
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            
+        } while(slow!=fast);
+        
+        
+        fast = nums[0];
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        
+        return slow;
+        
         
     }
 };
