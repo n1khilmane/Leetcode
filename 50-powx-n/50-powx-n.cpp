35e2c04206806f574d11381cac6f7cf177ebe75d
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-07-29 13:35:41 -0400

    Time: 7 ms (8.94%), Space: 5.9 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,7 +3,15 @@ public:
     double myPow(double x, int n) {
         double res = 1;
         while (n) {
-            if (n % 2) res = n > 0 ? res * x : res / x;
+            
+            if(n%2)
+            {
+                if(n>0)
+                    res=res*x;
+                else
+                    res=res/x;
+            }
+        
             x = x * x;
             n /= 2;
         }
