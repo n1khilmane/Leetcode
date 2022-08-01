8daf39b3d52ba2e3f5aa71b05700420149c30710
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-08-01 19:25:46 -0400

    Time: 114 ms (49.86%), Space: 13.2 MB (80.32%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        
-        sort(nums.begin(),nums.end());
-      
-       set<vector<int>> sv;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-    
-                for(int k=j+1;k<n;k++)
-                { 
-                  
-                   int x = (long long)target - 
-                           (long long)nums[i]-
-                           (long long)nums[j]-(long long)nums[k];
-                   
-                        if(binary_search(nums.begin()+k+1,nums.end(),x)){
-                            vector<int> v;
-                            v.push_back(nums[i]);
-                            v.push_back(nums[j]);
-                            v.push_back(nums[k]);
-                            v.push_back(x);
-                            sort(v.begin(),v.end());
-                            sv.insert(v);
-                        }
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i = nums.size() - 1; i > 2; i--) {
+            if (i < nums.size() - 1 && nums[i] == nums[i + 1]) continue;
+            for (int j = i - 1; j > 1; j--) {
+                if (j < i - 1 && nums[j] == nums[j + 1]) continue;
+                int l = 0, r = j - 1;
+                while (l < r) {
+                    long int sum = (long)nums[l] + nums[r] + nums[i] + nums[j];
+                    if (sum == target) {
+                        ans.push_back({nums[l], nums[r], nums[i], nums[j]});
+                        int num_left = nums[l], num_right = nums[r];
+                        while (l < r && nums[l] == num_left) l++;
+                        while (l < r && nums[r] == num_right) r--;
+                    }
+                    else {
+                        sum < target ? l++ : r--;
+                    }
                 }
             }
         }
-        vector<vector<int>> res(sv.begin(),sv.end());
-        return res;
+        return ans;
     }
 };
