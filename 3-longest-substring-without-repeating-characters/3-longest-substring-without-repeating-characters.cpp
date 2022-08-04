0048a565d32fbb40eeb4bcb076bfb8284e738c56
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-08-04 19:18:47 -0400

    Time: 37 ms (36.43%), Space: 10.8 MB (27.61%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,8 +5,9 @@ public:
 		int i = 0, j = 0, n = s.size(), ans = 0;
 		while(j<n){
 			if(set.find(s[j]) == set.end()){ //If the character is not in the set
-				set.insert(s[j++]); //Insert the character in set and update the j counter
-				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
+				set.insert(s[j]); //Insert the character in set and update the j counter
+				ans = max(ans, j-i+1);
+                j++;//Check if the new distance is longer than the current answer
 			}
 			else{
 				//If character does exist in the set, ie. it is a repeated character, 
