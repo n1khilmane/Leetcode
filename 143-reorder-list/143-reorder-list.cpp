15564ba99c6b35c5c58d3739b509814cff4586de
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2022-08-11 19:08:11 -0400

    Time: 57 ms (65.86%), Space: 17.8 MB (50.05%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -11,42 +11,30 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        if(!head || !head->next) return;
         
-        stack<ListNode*> st;
-        
-        ListNode *current = head;
-        
-        //add all the nodes to stack
-        while(current){
-            
-            st.push(current);
-            current = current->next; 
-            
+        ListNode *slow = head, *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *prev = nullptr, *curr = slow, *tmp;
+        while(curr){
+            tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
         }
         
-        // point to the start
-        current = head;
-        int n = st.size();
-        
-        if(n<3) return;
-        
-        ListNode* next;
-        
-        for(int i=0;i<n/2;++i)
-        {
-            next = current->next;
-            current->next = st.top();
-            st.pop();
+        ListNode *n1 = head, *n2 = prev;
+        while(n2->next){
+            tmp = n1->next;
+            n1->next = n2;
+            n1 = tmp;
             
-            current = current->next;
-            current->next = next;
-            current = current->next;
+            tmp = n2->next;
+            n2->next = n1;
+            n2 = tmp;
         }
-        
-        current->next = NULL;
-        
-        
-         
-        
     }
 };
