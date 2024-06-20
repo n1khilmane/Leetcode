47431aed914bf40b5c66ff05d43413845ba0a89f
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2024-06-19 20:55:40 -0400

    Time: 123 ms (69.44%), Space: 61.3 MB (73.97%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-    int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(), position.end());
-        int l = 0, r = position.back() - position.front();
-        while (l < r) {
-            int mid = r - (r - l) / 2;
-            if (count(position, mid) >= m)
-                l = mid;
-            else
-                r = mid - 1;
-        }
-        return l;
-    }
-private:
-    int count(vector<int>& position, int d) {
-        int ans = 1, cur = position[0];
-        for (int i = 1; i < position.size(); ++i) {
-            if (position[i] - cur >= d) {
-                ans++;
-                cur = position[i];
+    // Function to find the maximum minimum distance between m balls
+    int maxDistance(std::vector<int>& positions, int ballCount) {
+        // Sort positions to facilitate binary search
+        std::sort(positions.begin(), positions.end());
+        
+        // Initialize left and right bounds for binary search
+        int left = 0, right = positions.back() - positions.front();
+        
+        // Perform binary search to find the maximum minimum distance
+        while (left < right) {
+            int middle = right - (right - left) / 2;
+            
+            // If the number of balls that can be placed with distance 'middle' is greater or equal to ballCount
+            if (canPlaceBalls(positions, middle) >= ballCount) {
+                left = middle; // Try for a larger minimum distance
+            } else {
+                right = middle - 1; // Reduce the minimum distance
             }
         }
-        return ans;
+        
+        return left; // The largest minimum distance that can be achieved
+    }
+
+private:
+    // Helper function to count the number of balls that can be placed with at least 'distance' apart
+    int canPlaceBalls(const std::vector<int>& positions, int distance) {
+        int ballCounter = 1; // Start by placing the first ball at the first position
+        int currentPos = positions[0]; // The position of the last placed ball
+        
+        // Try to place the rest of the balls
+        for (size_t i = 1; i < positions.size(); ++i) {
+            // If the current position is at least 'distance' away from the last placed ball
+            if (positions[i] - currentPos >= distance) {
+                ballCounter++; // Place another ball here
+                currentPos = positions[i]; // Update the position of the last placed ball
+            }
+        }
+        
+        return ballCounter; // Return the number of balls placed
     }
 };
