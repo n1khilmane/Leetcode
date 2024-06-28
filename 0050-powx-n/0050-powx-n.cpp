2569ee0149506b36949bf5152075c956d9ffb8ab
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2024-06-28 00:53:03 -0400

    Time: 3 ms (37.11%), Space: 8.1 MB (44.75%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1;
+        while (n) {
+            
+            if(n%2)
+            {
+                if(n>0)
+                    res=res*x;
+                else
+                    res=res/x;
+            }
+        
+            x = x * x;
+            n /= 2;
+        }
+        return res;
+    }
+};
