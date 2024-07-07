7e2cbee01ae70ecdbee1ad774054a56e423f0333
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2024-07-06 20:31:46 -0400

    Time: 8 ms (32.06%), Space: 11.6 MB (55.31%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,37 +2,49 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-          map<int,int>m1; 
-    stack<int>st;
-     int n=nums2.size();
-    
-  for(int i=n-1;i>=0;i--){
-      
-      while(!st.empty() && st.top()<nums2[i]){
-          st.pop();
-      }
-
-      if(st.empty()){
-          m1[nums2[i]]=-1;
-      }
-      
-      else{
-          m1[nums2[i]]=st.top();
-      }
-      
-      st.push(nums2[i]);
-
-      
-  }
-    vector<int>ans;
-    
-    for(auto i:nums1){
-        ans.push_back(m1[i]);
-    }
-    
-    
-    return ans;
-}
+        map<int,int> mp;
+        vector<int> ans;
+        vector<int> temp;
+        stack<int> st;
+        int n = nums2.size();
         
-    
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.size()==0) temp.push_back(-1);
+            
+            else if(st.size() > 0 && st.top()>nums2[i])
+            {
+                temp.push_back(st.top());
+                
+            }
+            
+            else{
+                while(st.size() >0 && st.top()<nums2[i])
+                {
+                    st.pop();
+                }
+                
+                if(st.size()==0) temp.push_back(-1);
+                else temp.push_back(st.top());
+            }
+            st.push(nums2[i]);
+        }
+        
+        reverse(temp.begin(),temp.end());
+        
+        for(int i=0;i<n;i++)
+        {
+            mp[nums2[i]]=temp[i];
+        }
+        
+        for(int i=0;i<nums1.size();i++)
+        {
+            ans.push_back(mp[nums1[i]]);
+        }
+        
+        return ans;
+        
+        
+        
+    }
 };
