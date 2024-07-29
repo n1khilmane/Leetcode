ca571052d1d153a2c08372d580df09d10e7f37ca
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2024-07-28 21:25:29 -0400

    Time: 29 ms (36.38%), Space: 94.9 MB (44.50%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Get the sizes of both input arrays.
         int n = nums1.size();
         int m = nums2.size();
+        int i = 0, j = 0, m1 = 0, m2 = 0;
 
-        // Merge the arrays into a single sorted array.
-        vector<int> merged;
-        for (int i = 0; i < n; i++) {
-            merged.push_back(nums1[i]);
+        // Find median.
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
+            }
         }
-        for (int i = 0; i < m; i++) {
-            merged.push_back(nums2[i]);
-        }
 
-        // Sort the merged array.
-        sort(merged.begin(), merged.end());
-
-        // Calculate the total number of elements in the merged array.
-        int total = merged.size();
-
-        if (total % 2 == 1) {
-            // If the total number of elements is odd, return the middle element as the median.
-            return static_cast<double>(merged[total / 2]);
+        // Check if the sum of n and m is odd.
+        if ((n + m) % 2 == 1) {
+            return static_cast<double>(m1);
         } else {
-            // If the total number of elements is even, calculate the average of the two middle elements as the median.
-            int middle1 = merged[total / 2 - 1];
-            int middle2 = merged[total / 2];
-            return (static_cast<double>(middle1) + static_cast<double>(middle2)) / 2.0;
+            double ans = static_cast<double>(m1) + static_cast<double>(m2);
+            return ans / 2.0;
         }
     }
 };
