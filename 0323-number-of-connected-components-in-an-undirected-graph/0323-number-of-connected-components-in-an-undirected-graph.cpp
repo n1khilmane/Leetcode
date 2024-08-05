141c1ee88155f6c0e15f9d552ae299f9be06c3d7
Nikhil Mane <31525500+n1khilmane@users.noreply.github.com>
2024-08-05 16:45:38 -0400

    Time: 8 ms (96.96%), Space: 17.2 MB (54.50%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(vector<int> adjList[], vector<int> &visited, int src) {
+        visited[src] = 1;
+        
+        for (int i = 0; i < adjList[src].size(); i++) {
+            if (visited[adjList[src][i]] == 0) {
+                dfs(adjList, visited, adjList[src][i]);
+            }
+        }
+    }
+    
+    int countComponents(int n, vector<vector<int>>& edges) {
+        if (n == 0) return 0;
+      
+        int components = 0;
+        vector<int> visited(n, 0);
+        vector<int> adjList[n];
+    
+        for (int i = 0; i < edges.size(); i++) {
+            adjList[edges[i][0]].push_back(edges[i][1]);
+            adjList[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                components++;
+                dfs(adjList, visited, i);
+            }
+        }
+        return components;
+    }
+};
